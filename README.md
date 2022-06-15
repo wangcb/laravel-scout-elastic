1f7d429c0bf5c8db6990b3544c4448563eedd1cd
wangcb <wangcb615@163.com>
2022-06-15 17:51:43 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# laravel-scout-elastic
+# Laravel Scout Elasticsearch Driver
+
+[![Latest Stable Version](https://github.com/wangcb/laravel-scout-elastic)](//packagist.org/packages/wangcb/laravel-scout-elastic) [![Total Downloads](https://poser.pugx.org/tamayo/laravel-scout-elastic/downloads)](//packagist.org/packages/tamayo/laravel-scout-elastic) [![License](https://poser.pugx.org/tamayo/laravel-scout-elastic/license)](//packagist.org/packages/tamayo/laravel-scout-elastic) ![Build Status](https://travis-ci.org/ErickTamayo/laravel-scout-elastic.svg?branch=master)
+
+This package provides a [Elasticsearch](https://www.elastic.co/products/elasticsearch) driver for Laravel Scout.
+
+## Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require tamayo/laravel-scout-elastic
+```
+
+Laravel will automatically register the driver service provider.
+
+#### Install elasticsearch-php client
+
+For use this library we recomend using the latest version at this time `(^7.9)`
+
+```bash
+composer require elasticsearch/elasticsearch
+```
+
+### Setting up Elasticsearch configuration
+
+After you've published the Laravel Scout package configuration, you need to set your driver to `elasticsearch` and add its configuration:
+
+```php
+// config/scout.php
+...
+    // Set your driver to elasticsearch
+    'driver' => env('SCOUT_DRIVER', 'elasticsearch'),
+...
+    /*
+    |--------------------------------------------------------------------------
+    | Elasticsearch Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Elasticsearch settings.
+    |
+    */
+    'elasticsearch' => [
+        'hosts' => [
+            env('ELASTICSEARCH_HOST', 'localhost'),
+            // [
+            //     'host'   => env('ELASTICSEARCH_HOST', 'localhost'),
+            //     'port'   => env('ELASTICSEARCH_PORT', '9200'),
+            //     'scheme' => env('ELASTICSEARCH_SCHEME', 'https'),
+            //     'path'   => env('ELASTICSEARCH_PATH', '/elastic'),
+            //     'user'   => env('ELASTICSEARCH_USER', 'username'),
+            //     'pass'   => env('ELASTICSEARCH_PASS', 'password'),
+            // ]
+        ],
+    ]
+...
+```
+
+For host configuration you can refer to the official [Elasticsearch documentation](https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html)
+
+## Usage
+
+Now you can use Laravel Scout as described in the [Laravel Scout official documentation](https://laravel.com/docs/8.x/scout)
+
+## Limitations
+
+**Identifying Users**
+Currrently user identification is not supported.
+
+## Credits
+
+- [Erick Tamayo](https://github.com/ericktamayo)
+- [All Contributors](../../contributors)
+
+## License
+
+The MIT License (MIT).
---
+++ src/Engines/ElasticsearchEngine.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace Tamayo\LaravelScoutElastic\Engines;
+
+use Laravel\Scout\Builder;
+use Laravel\Scout\Engines\Engine;
+use Elasticsearch\Client as Elastic;
+use Illuminate\Database\Eloquent\Collection;
+
+class ElasticsearchEngine extends Engine
+{
+    /**
+     * Elastic client.
+     *
+     * @var Elastic
+     */
+    protected $elastic;
+
+    /**
+     * Create a new engine instance.
+     *
+     * @param  \Elasticsearch\Client  $elastic
+     * @return void
+     */
+    public function __construct(Elastic $elastic)
+    {
+        $this->elastic = $elastic;
+    }
+
+    public function lazyMap(Builder $builder, $results, $model)
+    {
+        return Collection::make($results['hits']['hits'])->map(function ($hit) use ($model) {
+            return $model->newFromBuilder($hit['_source']);
+        });
+    }
+
+    public function createIndex($name, array $options = [])
+    {
+        $params = [
+            'index' => $name,
+        ];
+
+        if (isset($options['shards'])) {
+            $params['body']['settings']['number_of_shards'] = $options['shards'];
+        }
+
+        if (isset($options['replicas'])) {
+            $params['body']['settings']['number_of_replicas'] = $options['replicas'];
+        }
+
+        $this->elastic->indices()->create($params);
+    }
+
+    public function deleteIndex($name)
+    {
+        $this->elastic->indices()->delete(['index' => $name]);
+    }
+
+    /**
+     * Update the given model in the index.
+     *
+     * @param  Collection  $models
+     * @return void
+     */
+    public function update($models)
+    {
+        if ($models->isEmpty()) {
+            return;
+        }
+
+        $params['body'] = [];
+
+        $models->each(function ($model) use (&$params) {
+            $params['body'][] = [
+                'update' => [
+                    '_id' => $model->getScoutKey(),
+                    '_index' => $model->searchableAs(),
+                    '_type' => get_class($model),
+                ]
+            ];
+            $params['body'][] = [
+                'doc' => $model->toSearchableArray(),
+                'doc_as_upsert' => true
+            ];
+        });
+
+        $this->elastic->bulk($params);
+    }
+
+    /**
+     * Remove the given model from the index.
+     *
+     * @param  Collection  $models
+     * @return void
+     */
+    public function delete($models)
+    {
+        $params['body'] = [];
+
+        $models->each(function ($model) use (&$params) {
+            $params['body'][] = [
+                'delete' => [
+                    '_id' => $model->getKey(),
+                    '_index' => $model->searchableAs(),
+                    '_type' => get_class($model),
+                ]
+            ];
+        });
+
+        $this->elastic->bulk($params);
+    }
+
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  Builder  $builder
+     * @return mixed
+     */
+    public function search(Builder $builder)
+    {
+        return $this->performSearch($builder, array_filter([
+            'numericFilters' => $this->filters($builder),
+            'size' => $builder->limit,
+        ]));
+    }
+
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  Builder  $builder
+     * @param  int  $perPage
+     * @param  int  $page
+     * @return mixed
+     */
+    public function paginate(Builder $builder, $perPage, $page)
+    {
+        $result = $this->performSearch($builder, [
+            'numericFilters' => $this->filters($builder),
+            'from' => (($page * $perPage) - $perPage),
+            'size' => $perPage,
+        ]);
+
+        $result['nbPages'] = $result['hits']['total']['value'] / $perPage;
+
+        return $result;
+    }
+
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  Builder  $builder
+     * @param  array  $options
+     * @return mixed
+     */
+    protected function performSearch(Builder $builder, array $options = [])
+    {
+        $params = [
+            'index' => $builder->model->searchableAs(),
+            'type' => get_class($builder->model),
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'must' => [['query_string' => ['query' => "*{$builder->query}*"]]]
+                    ]
+                ]
+            ]
+        ];
+
+        if ($sort = $this->sort($builder)) {
+            $params['body']['sort'] = $sort;
+        }
+
+        if (isset($options['from'])) {
+            $params['body']['from'] = $options['from'];
+        }
+
+        if (isset($options['size'])) {
+            $params['body']['size'] = $options['size'];
+        }
+
+        if (isset($options['numericFilters']) && count($options['numericFilters'])) {
+            $params['body']['query']['bool']['must'] = array_merge(
+                $params['body']['query']['bool']['must'],
+                $options['numericFilters']
+            );
+        }
+
+        if ($builder->callback) {
+            return call_user_func(
+                $builder->callback,
+                $this->elastic,
+                $builder->query,
+                $params
+            );
+        }
+
+        return $this->elastic->search($params);
+    }
+
+    /**
+     * Get the filter array for the query.
+     *
+     * @param  Builder  $builder
+     * @return array
+     */
+    protected function filters(Builder $builder)
+    {
+        return collect($builder->wheres)->map(function ($value, $key) {
+            if (is_array($value)) {
+                return ['terms' => [$key => $value]];
+            }
+
+            return ['match_phrase' => [$key => $value]];
+        })->values()->all();
+    }
+
+    /**
+     * Pluck and return the primary keys of the given results.
+     *
+     * @param  mixed  $results
+     * @return \Illuminate\Support\Collection
+     */
+    public function mapIds($results)
+    {
+        return collect($results['hits']['hits'])->pluck('_id')->values();
+    }
+
+    /**
+     * Map the given results to instances of the given model.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @param  mixed  $results
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return Collection
+     */
+    public function map(Builder $builder, $results, $model)
+    {
+        if ($results['hits']['total'] === 0) {
+            return $model->newCollection();
+        }
+
+        $keys = collect($results['hits']['hits'])->pluck('_id')->values()->all();
+
+        $modelIdPositions = array_flip($keys);
+
+        return $model->getScoutModelsByIds(
+            $builder,
+            $keys
+        )->filter(function ($model) use ($keys) {
+            return in_array($model->getScoutKey(), $keys);
+        })->sortBy(function ($model) use ($modelIdPositions) {
+            return $modelIdPositions[$model->getScoutKey()];
+        })->values();
+    }
+
+    /**
+     * Get the total count from a raw result returned by the engine.
+     *
+     * @param  mixed  $results
+     * @return int
+     */
+    public function getTotalCount($results)
+    {
+        return $results['hits']['total']['value'];
+    }
+
+    /**
+     * Flush all of the model's records from the engine.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function flush($model)
+    {
+        $model->newQuery()
+            ->orderBy($model->getKeyName())
+            ->unsearchable();
+    }
+
+    /**
+     * Generates the sort if theres any.
+     *
+     * @param  Builder $builder
+     * @return array|null
+     */
+    protected function sort($builder)
+    {
+        if (count($builder->orders) == 0) {
+            return null;
+        }
+
+        return collect($builder->orders)->map(function ($order) {
+            return [$order['column'] => $order['direction']];
+        })->toArray();
+    }
+}
